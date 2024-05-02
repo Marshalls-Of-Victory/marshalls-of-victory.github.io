a3a1e0f9fd1ee3e30ea638aa1a31502db2eb7238
Paprykarz-Lua <pslawinskiiii@gmail.com>
2024-05-02 02:28:39 +0200

    the begging of mulit-language system

--- js/scripts.js
+++ js/scripts.js
@@ -45,3 +45,55 @@ window.addEventListener('DOMContentLoaded', event => {
 });
 
 
+pageText = [
+    { //English
+        "Section_1_Title" : "Photos",
+        "Section_2_Title" : "Bio",
+        "Section_3_Title" : "Contact us"
+    },
+
+    { //Polski
+        "Section_1_Title" : "Zdjęcia",
+        "Section_2_Title" : "Bio",
+        "Section_3_Title" : "Skontaktuj się z nami"
+    }
+]
+
+
+if (localStorage.getItem("lang") == "" || localStorage.getItem("lang") == undefined) {
+    localStorage.setItem("lang", 0);
+}
+
+console.log("język: "  + localStorage.getItem("lang"));
+function refreshFlags() {
+    for (let i = 0; i < 2; i++) {
+        if (localStorage.getItem("lang") == i) {
+            document.getElementById("lang-"+i).style.borderWidth = "3px";
+        } else {
+            document.getElementById("lang-"+i).style.borderWidth = "0px";
+        }
+    }
+}
+
+refreshFlags();
+
+
+function langClicked(lang) {
+    localStorage.setItem("lang", lang);
+    console.log("lang: " + lang)
+    location.reload();
+}
+
+function translate() {
+    let langPack = pageText[localStorage.getItem("lang")];
+    for (const [k, v] of Object.entries(langPack)) {
+        console.log(k + ": " + v);
+        let elms = document.getElementsByClassName(k);
+        for (let i = 0; i < elms.length; i++) {
+            elms[i].innerHTML = v;
+        }
+    }
+}
+
+translate()
+
