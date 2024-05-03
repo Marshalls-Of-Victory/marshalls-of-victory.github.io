57284ad45dae2ff1c799e91de1ed2174ec819d14
Paprykarz-Lua <pslawinskiiii@gmail.com>
2024-05-03 22:50:31 +0200

    Rearranged structure, finished translating

--- js/scripts.js
+++ js/scripts.js
@@ -49,13 +49,27 @@ pageText = [
     { //English
         "Section_1_Title" : "Photos",
         "Section_2_Title" : "Bio",
-        "Section_3_Title" : "Contact us"
+        "Section_3_Title" : "Contact us",
+        "send_message" : "Send a message",
+        "input_name" : "Name",
+        "input_message" : "Message",
+        "bio_1" : "We are Marshalls of Victory, a young band from Cracow, Poland that was formed at the beginning of March 2024. We play many genres of music, such as blues rock, hardrock, punk, punkrock, metal.",
+        "bio_2" : "Band members: <br> - Patryk Sławiński - Lead guitar <br> - Jan Krawczyk - Bass guitar and vocals <br> - Alex Nemertsalov - Rhythm guitar <br> - Antek Otwinowski - drums",
+        "footer_location_label" : "Location",
+        "footer_location" : "Cracow, Poland"
     },
 
     { //Polski
         "Section_1_Title" : "Zdjęcia",
         "Section_2_Title" : "Bio",
-        "Section_3_Title" : "Skontaktuj się z nami"
+        "Section_3_Title" : "Skontaktuj się z nami",
+        "send_message" : "Wyślij",
+        "input_name" : "Imię i nazwisko",
+        "input_message" : "Wiadomość",
+        "bio_1" : "Nazywamy się Marshalls of Victory, jesteśmy młodym zespołem z Krakowa, który powstał na początku marca 2024 roku. Gramy wiele gatunków muzyki, takich jak od blues rock, hardrock, punk, punkrock, metal.",
+        "bio_2" : "Skład zespołu: <br> - Patryk Sławiński - Gitara prowadząca <br> - Jan Krawczyk - Gitara basowa oraz wokal <br> - Alex Nemertsalov - Gitara rytmiczna oraz wokal wspierający <br> - Antek Otwinowski - perkusja",
+        "footer_location_label" : "Lokalizacja",
+        "footer_location" : "Kraków, Polska"
     }
 ]
 
@@ -64,7 +78,6 @@ if (localStorage.getItem("lang") == "" || localStorage.getItem("lang") == undefi
     localStorage.setItem("lang", 0);
 }
 
-console.log("język: "  + localStorage.getItem("lang"));
 function refreshFlags() {
     for (let i = 0; i < 2; i++) {
         if (localStorage.getItem("lang") == i) {
@@ -72,6 +85,7 @@ function refreshFlags() {
         } else {
             document.getElementById("lang-"+i).style.borderWidth = "0px";
         }
+
     }
 }
 
@@ -80,17 +94,24 @@ refreshFlags();
 
 function langClicked(lang) {
     localStorage.setItem("lang", lang);
-    console.log("lang: " + lang)
     location.reload();
 }
 
 function translate() {
     let langPack = pageText[localStorage.getItem("lang")];
     for (const [k, v] of Object.entries(langPack)) {
-        console.log(k + ": " + v);
         let elms = document.getElementsByClassName(k);
         for (let i = 0; i < elms.length; i++) {
             elms[i].innerHTML = v;
+
+        }
+        elms = [...document.getElementsByTagName("input"), ...document.getElementsByTagName("textarea")];
+        for (let i = 0; i < elms.length; i++) {
+            console.log(elms[i].getAttribute("placeholder"));
+            if (elms[i].getAttribute("placeholder") == k) {
+                console.log("found");
+                elms[i].setAttribute("placeholder", v);
+            }
         }
     }
 }
