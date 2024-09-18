68ede6d1f1cde519871aae886020f9b655289a2e
Paprykarz-Lua <pslawinskiiii@gmail.com>
2024-09-18 22:10:08 +0200

    Testing module

---
+++ modules/footer.js
@@ -0,0 +1,29 @@
+document.write(`
+        <footer class="footer text-center"></footer>
+            <div class="container">
+                <div class="row" style = "text-align: center;">
+                    <!-- Footer Location-->
+                    <div class="col-lg-6 mb-5 mb-lg-0">
+                        <h4 class="text-uppercase mb-4"><span class = "footer_location_label"></span></h4>
+                        <p class="lead mb-0">
+                            <span class = "footer_location"></span>
+                        </p>
+                    </div>
+                    <!-- Footer Social Icons-->
+                    <div class="col-lg-6 mb-5 mb-lg-0">
+                        <h4 class="text-uppercase mb-4">Social Media</h4>
+                        <a class="btn btn-outline-light btn-social mx-1" href="https://www.instagram.com/marshallsofvictory"><i class="fab fa-fw fa-instagram"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="https://www.facebook.com/profile.php?id=61558754971767"><i class="fab fa-fw fa-facebook-f"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="https://twitter.com/movtheband"><i class="fab fa-fw fa fa-brands fa-twitter"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="https://www.tiktok.com/@marshallsofvictory"><i class="fab fa-fw fa-tiktok"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="https://www.youtube.com/@MarshallsOfVictory"><i class="fab fa-fw fa fa-brands fa-youtube"></i></a>
+                    </div>
+                    <!-- Footer About Text-->
+                </div>
+            </div>
+        </footer>
+        <!-- Copyright Section-->
+        <div class="copyright py-4 text-center text-white">
+            <div class="container"><small>Copyright &copy; Marshalls Of Victory 2024</small></div>
+        </div>    
+`)
